7908143b576c199aeb040f1e211aa733e58ff2db
Zak Thompson <zak@thisfireinside.com>
2019-06-01 16:32:53 -0400

    Window not available at build, use react-media instead

--- src/components/skewedBackground.js
+++ src/components/skewedBackground.js
@@ -1,40 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Media from 'react-media';
 import styles from './skewedBackground.module.css';
 
-const skewedBackground = ({ className, color, offset, children }) => {
-  let offsetFactor = 1;
-  if (window.innerWidth < 1280) {
-    offsetFactor = 0.75;
-  }
-  if (window.innerWidth < 640) {
-    offsetFactor = 0.25;
-  }
+const skewedBackground = ({ className, color, offset, children }) => (
+  <Media query="(max-width: 640px)">
+    {matches => {
+      let offsetFactor = 1;
 
-  const modifiedOffset = offset * offsetFactor;
+      if (matches) {
+        offsetFactor = 0.25;
+      }
 
-  return (
-    <div
-      className={styles.wrapper}
-      style={{
-        marginLeft: `${modifiedOffset}px`,
-        marginRight: `${modifiedOffset < 0 ? Math.abs(modifiedOffset) : `-${modifiedOffset}`}px`,
-      }}
-    >
-      <div className={styles.content}>
-        {children}
-      </div>
-      <div
-        className={styles.background}
-        style={{
-          backgroundColor: color,
-        }}
-      >
-      </div>
-    </div>
-  );
-}
+      const modifiedOffset = offset * offsetFactor;
 
+      return (
+        <div
+          className={styles.wrapper}
+          style={{
+            marginLeft: `${modifiedOffset}px`,
+            marginRight: `${modifiedOffset < 0 ? Math.abs(modifiedOffset) : `-${modifiedOffset}`}px`,
+          }}
+        >
+          <div className={styles.content}>
+            {children}
+          </div>
+          <div
+            className={styles.background}
+            style={{
+              backgroundColor: color,
+            }}
+          >
+          </div>
+        </div>
+      );
+    }}
+  </Media>
+)
 
 skewedBackground.propTypes = {
   color: PropTypes.string.isRequired,
