c9e3d262c3227b71b3e90a12e224999bb68263bd
zakthompson <zak@thisfireinside.com>
2020-01-16 11:10:23 -0500

    Add ESL advertisement, fix header

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from "react"
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types"
 import { StaticQuery, Link, graphql } from "gatsby"
-import Img from 'gatsby-image';
-import styles from './header.module.css';
+import Img from "gatsby-image"
+import styles from "./header.module.css"
 
 const Header = ({ isHome }) => {
   const [scrolled, setScrolled] = useState(false)
 
   function onScroll() {
     if (!scrolled && window.scrollY >= 200) {
-      setScrolled(true);
+      setScrolled(true)
     } else if (window.scrollY < 200) {
-      setScrolled(false);
+      setScrolled(false)
     }
   }
 
@@ -19,56 +19,64 @@ const Header = ({ isHome }) => {
     window.scrollTo({
       top: document.body.scrollHeight,
       left: 0,
-      behavior: 'smooth',
-    });
+      behavior: "smooth",
+    })
   }
 
   useEffect(() => {
-    window.addEventListener('scroll', onScroll);
+    window.addEventListener("scroll", onScroll)
     return () => {
-      window.removeEventListener('scroll', onScroll);
-    };
-  }, []);
+      window.removeEventListener("scroll", onScroll)
+    }
+  }, [])
 
-  return (<StaticQuery
-    query={query}
-    render={data => (
-      <header className={`${styles.header}${scrolled ? ` ${styles.scrolled}` : ''}`}>
-        <Link to="/">
-          <Img
-            fixed={data.logoImage.childImageSharp.fixed}
-          />
-        </Link>
-        {(isHome || scrolled) && <div className={styles.name}>
-          <h6>Queensway</h6>
-          <h6>Baptist Church</h6>
-        </div>}
-        <div className={styles.fill} />
-        <Link className={styles.link} to="/sermons">Sermons</Link>
-        <div className={styles.visit} onClick={scrollDown}>Plan a Visit</div>
-      </header>
-    )}
-  />);
+  return (
+    <StaticQuery
+      query={query}
+      render={data => (
+        <header
+          className={`${styles.header}${scrolled ? ` ${styles.scrolled}` : ""}`}
+        >
+          <Link to="/">
+            <Img fixed={data.logoImage.childImageSharp.fixed} />
+            {(isHome || scrolled) && (
+              <div className={styles.name}>
+                <h6>Queensway</h6>
+                <h6>Baptist Church</h6>
+              </div>
+            )}
+          </Link>
+          <div className={styles.fill} />
+          <Link className={styles.link} to="/sermons">
+            Sermons
+          </Link>
+          <div className={styles.visit} onClick={scrollDown}>
+            Plan a Visit
+          </div>
+        </header>
+      )}
+    />
+  )
 }
 
 Header.propTypes = {
   isHome: PropTypes.bool,
-};
+}
 
 Header.defaultProps = {
   isHome: false,
-};
+}
 
 export const query = graphql`
   query {
     logoImage: file(relativePath: { eq: "logo.png" }) {
       childImageSharp {
         fixed(width: 80, height: 80) {
-          ...GatsbyImageSharpFixed,
-        },
-      },
-    },
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
   }
-`;
+`
 
 export default Header
