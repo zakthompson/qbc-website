c2927a9ddd30ec94beb5e009e815afa480232f3d
zakthompson <zak@thisfireinside.com>
2020-01-16 11:29:47 -0500

    Gatsby-friendly alternative way to handle ESL image

--- src/components/about.js
+++ src/components/about.js
@@ -1,51 +1,43 @@
 import React from "react"
 import BgImg from "gatsby-background-image"
 import { StaticQuery, graphql } from "gatsby"
-import { useMediaQuery } from "beautiful-react-hooks"
 import SkewedBackground from "./skewedBackground"
 import styles from "./about.module.css"
 
-const About = () => {
-  const isSmall = useMediaQuery("(max-width: 1024px)")
-  console.log(isSmall)
-  return (
-    <StaticQuery
-      query={query}
-      render={data => {
-        const text = data.directusPastorsMessage.text.split("\n").join("<br />")
-        return (
-          <section className={styles.wrap}>
-            <div className={styles.about}>
-              <SkewedBackground color="#222">
-                <h2>{data.directusPastorsMessage.headline}</h2>
-              </SkewedBackground>
-              <div
-                className={styles.aboutText}
-                dangerouslySetInnerHTML={{
-                  __html: text,
-                }}
-              />
-            </div>
-            <div className={styles.image}>
-              <BgImg
-                key={isSmall}
-                className={styles.imageWrap}
-                Tag="div"
-                fluid={data.image.childImageSharp.fluid}
-                style={{
-                  backgroundSize: "contain",
-                  backgroundPosition: isSmall
-                    ? "center bottom"
-                    : "right center",
-                }}
-              />
-            </div>
-          </section>
-        )
-      }}
-    />
-  )
-}
+const About = () => (
+  <StaticQuery
+    query={query}
+    render={data => {
+      const text = data.directusPastorsMessage.text.split("\n").join("<br />")
+      return (
+        <section className={styles.wrap}>
+          <div className={styles.about}>
+            <SkewedBackground color="#222">
+              <h2>{data.directusPastorsMessage.headline}</h2>
+            </SkewedBackground>
+            <div
+              className={styles.aboutText}
+              dangerouslySetInnerHTML={{
+                __html: text,
+              }}
+            />
+          </div>
+          <div className={styles.image}>
+            <BgImg
+              className={styles.imageWrap}
+              Tag="div"
+              fluid={data.image.childImageSharp.fluid}
+              style={{
+                backgroundSize: "",
+                backgroundPosition: "",
+              }}
+            />
+          </div>
+        </section>
+      )
+    }}
+  />
+)
 
 export const query = graphql`
   query {
