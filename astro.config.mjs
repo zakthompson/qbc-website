af452e49f72afff4841990b98972cd6e7c8395ad
zakthompson <zak@thisfireinside.com>
2022-11-18 00:23:44 -0500

    Astro rewrite

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
+  vite: {
+    ssr: {
+      noExternal: ["@astro-community/astro-embed-youtube"],
+    },
+  },
+});
